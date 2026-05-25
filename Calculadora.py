1b8f70ffd49ff2524a253dee7fafe754d5f5a4fd
Alejandro Velandia <alejitovj@gmail.com>
2026-05-24 21:35:30 -0500

    .

---
+++ Calculadora.py
@@ -0,0 +1,32 @@
+num1 = float(input("Ingrese el primer numero: "))
+num2 = float(input("Ingrese el primer numero: "))
+
+print("Seleccione una operacion: ")
+print("1. Sumar: ")
+print("2. Restar: ")
+print("3. Multiplicar: ")
+print("4. Dividir: ")
+
+opcion = input("Digite una opcion 1(/2/3/4): ")
+
+if opcion == "1":
+    resultado = num1 + num2
+    print("La suma es: ", resultado)
+
+elif opcion == "2":
+    resultado = num1 - num2
+    print("La resta es: ", resultado)
+
+elif opcion == "3":
+    resultado = num1 * num2
+    print("La multiplicacion es: ", resultado)
+
+elif opcion == "4":
+    if num2 != 0:
+        resultado = num1 / num2
+        print("La division es: ", resultado)
+    else:
+        print("No se puede dividir entre cero ")
+
+else:
+    print("Opcion no válida")
