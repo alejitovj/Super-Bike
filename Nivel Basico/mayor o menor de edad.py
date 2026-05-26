3223bdb8777fdab4286082d8ba872ab71f7139b0
Alejandro Velandia <alejitovj@gmail.com>
2026-05-25 21:21:58 -0500

    .

---
+++ Nivel Basico/mayor o menor de edad.py
@@ -0,0 +1,14 @@
+# Determinar mayoria o menoria de edad, dependiendo de la misma #
+
+nombre = input("Digite su nombre por favor: ")
+
+print("Depende de la edad suya, se determina si usted es menor o mayor de edad")
+
+edad = int(input("Ahora Digite su edad: "))
+
+
+if  edad <= 17:
+    print("Usted es menor de edad")
+else:
+    print("Usted es mayor de edad")
+
