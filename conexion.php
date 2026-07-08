fdde8d3fb52236a2c303bc8094170370da838911
Alejandro Velandia <alejitovj@gmail.com>
2026-07-08 18:54:03 -0500

    Initial commit

---
+++ conexion.php
@@ -0,0 +1,19 @@
+<?php
+
+$_SERVER = "localhost";
+$user = "root";
+$pass ="";
+$db = "usuarios";
+
+$conexion = new mysqli($_SERVER, $user, $pass, $db);
+
+if ($conexion->connect_errno) {
+    die("Conexion Fallida" . $conexion->connect_errno );
+
+}   else {
+    echo"conectado";
+}
+
+
+
+?>
