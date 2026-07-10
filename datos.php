f3c46c3cda519c024acbe09fde3d82746bc33fd9
Alejandro Velandia <alejitovj@gmail.com>
2026-07-09 20:28:01 -0500

    .

--- datos.php
+++ datos.php
@@ -1,21 +1,19 @@
 <?php
-// 1. Conectamos a la base de datos
 include("conexion.php");
 
-// 2. Capturamos los datos del formulario (deben coincidir con el 'name' en HTML)
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
 $usuario = $_POST['usuario'];
 $password = $_POST['password'];
 
-// 3. Insertamos los datos en el orden correcto
 $sql = "INSERT INTO usuarios (nombre, email, nombre_usuario, password) 
         VALUES ('$nombre', '$email', '$usuario', '$password')";
 
-// 4. Verificamos si funcionó
 if ($conexion->query($sql) === TRUE) {
-    echo "¡Registro exitoso! Tus datos ya están en la base de datos.";
+    // Redirigimos a una página de éxito (crearemos este archivo abajo)
+    header("Location: exito.html");
+    exit(); 
 } else {
-    echo "Error: " . $sql . "<br>" . $conexion->error;
+    echo "Error en el registro: " . $conexion->error;
 }
 ?>
